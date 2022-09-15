ace1d428fb2e8f6314cc78f5e4f91d16d20f0d66
nikmatulmaula <anikmaula23@gmail.com>
2022-09-15 16:46:32 +0700

    upload

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Latihan1 extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Codeigniter : Write Less Do More
+    }
+
+    function index()
+    {
+        echo "Selamat Belajar Web Programming 2 Dengan Kelas 12.2A.35";
+    }
+
+}
